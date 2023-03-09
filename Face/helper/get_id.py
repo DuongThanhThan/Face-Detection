d35ae32591924f10c322bd5a7babc37f01c592d5
phat.pham <phat.pham@terralogic.com>
2023-03-09 17:14:15 +0700

    Refactoring codes

--- Face/helper/get_id.py
+++ Face/helper/get_id.py
@@ -6,6 +6,8 @@ def get_id(img_path) :
     user_id = Face.face_recognition(img_path)
     return user_id
 
-
 if __name__ == '__main__':
-    print(get_id("../../Face/Face_Model/input/TheRock.jpeg"))
+    import os
+    parent = parent = os.path.join(os.getcwd(), os.pardir)
+    parent_path = os.path.abspath(parent)
+    print(get_id(parent_path+"/Face_Model/input/test.mp4"))
